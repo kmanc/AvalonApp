3e6aca8a65a0d7411856f1cd82004b6945ef70f1
Kevin <kmanc@comcast.net>
2016-02-13 14:35:56 -0500

    Most updated version

--- src/main/java/koins/avalonapp/Game_Setup.java
+++ src/main/java/koins/avalonapp/Game_Setup.java
@@ -208,9 +208,8 @@ public class Game_Setup extends AppCompatActivity implements View.OnClickListene
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // Handle action bar item clicks here. The action bar will automatically handle clicks
+        // on the Home/Up button, so long as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
